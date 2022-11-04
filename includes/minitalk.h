49f03a0a4824e8bd061b2480441a5d0c8a2e5d76
Edu <bfwaend@gmail.com>
2022-11-04 00:01:08 -0300

    Created some code to test makefile

---
+++ includes/minitalk.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 23:53:29 by edu               #+#    #+#             */
+/*   Updated: 2022/11/03 23:56:01 by edu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include "../libft/includes/libft.h"
+# include "../libft/includes/get_next_line.h"
+# include "../libft/includes/ft_printf.h"
+# include "../libft/includes/libput.h"
+#endif
