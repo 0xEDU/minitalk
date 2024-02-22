c5ebdc8c25908298f0a29f661c4945d07a4cf616
etachott <bfwaend@gmail.com>
2024-02-21 22:48:34 -0300

    (refactor): Reduce client.c

--- sources/client.c
+++ sources/client.c
@@ -6,69 +6,50 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 23:52:31 by edu               #+#    #+#             */
-/*   Updated: 2022/11/10 18:03:31 by etachott         ###   ########.fr       */
+/*   Updated: 2024/02/21 22:43:15 by etachott         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	better_kill(int pid, int signal)
+int	send_message(char *str, int server_pid)
 {
-	kill(pid, signal);
-	usleep(100);
-}
-
-int	terminate_string(int pid)
-{	
 	static int	index = 0;
-
-	if (index++ != 8)
-	{
-		kill(pid, SIGUSR1);
-		return (0);
-	}
-	return (1);
-}
-
-int	send_message(char *str, int pid)
-{
+	static int	msg_bit_pos = -1;
 	static char	*msg = 0;
-	static int	server_pid = 0;
-	static int	index = -1;
 
 	if (str)
-		msg = ft_strdup(str);
-	if (!msg)
-		ft_printf("abacate\n");
-	if (pid)
-		server_pid = pid;
-	if (msg[++index / 8])
 	{
-		if (msg[index / 8] & (128 >> (index % 8)))
-			better_kill(server_pid, SIGUSR2);
+		msg = str;
+		msg[ft_strlen(str) + 1] = '\0';
+	}
+	if (msg[++msg_bit_pos / 8])
+	{
+		if (msg[msg_bit_pos / 8] & (128 >> (msg_bit_pos % 8)))
+			kill(server_pid, SIGUSR2);
 		else
-			better_kill(server_pid, SIGUSR1);
+			kill(server_pid, SIGUSR1);
 		return (0);
 	}
-	if (!terminate_string(server_pid))
+	if (index++ != 8)
+	{
+		kill(server_pid, SIGUSR1);
 		return (0);
-	free(msg);
+	}
+	msg = NULL;
 	return (1);
 }
 
 void	handler(int signal, siginfo_t *info, void *ucontext)
 {
-	int	end;
-
-	end = 0;
 	if (signal == SIGUSR1)
-		end = send_message(0, 0);
-	if (end)
 	{
-		ft_printf("Success!\n");
-		exit(0);
+		if (send_message(0, info->si_pid))
+		{
+			ft_printf("Success!\n");
+			exit(0);
+		}
 	}
-	(void)info;
 	(void)ucontext;
 }
 
@@ -76,18 +57,18 @@ int	main(int argc, char *argv[])
 {	
 	struct sigaction	sigact;
 	sigset_t			set;
-	int					pid;
+	int					server_pid;
 
 	if (argc != 3)
 		return (ft_printf("Usage: ./client [PID] [STRING]\n"));
-	pid = ft_atoi(argv[1]);
+	server_pid = ft_atoi(argv[1]);
 	sigemptyset(&set);
 	sigact.sa_mask = set;
 	sigact.sa_flags = 0;
 	sigact.sa_sigaction = handler;
 	sigaction(SIGUSR1, &sigact, NULL);
 	sigaction(SIGUSR2, &sigact, NULL);
-	send_message(argv[2], pid);
+	send_message(argv[2], server_pid);
 	while (1)
 		pause();
 	return (0);
